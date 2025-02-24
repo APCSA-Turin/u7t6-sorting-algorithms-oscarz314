ebeb5b41677366cf67692ffacac06db26746b739
Oscarz <111668068+oscarz314@users.noreply.github.com>
2025-02-24 17:00:18 +0000

    hello

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,15 +8,48 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-       
-        return elements;
+       int minIndex = 0;
+
+       for(int i = 0; i < elements.length - 1; i++){
+        for (int j = i + 1; j < elements.length; j++){
+            if (elements[j] < elements[minIndex]){
+                minIndex = j;
+            }
+        }
+        int temp = elements[minIndex];
+        elements[minIndex] = elements[i];
+        elements[i] = temp;
+        minIndex = i + 1;
+       }
+
+       return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-       
-        return words;
+        int minIndex = 0;
+
+       for(int i = 0; i < words.size() - 1; i++){
+        for (int j = i + 1; j < words.size(); j++){
+            if (words.get(j).compareTo(words.get(minIndex)) < 0){
+                minIndex = j;
+            }
+        }
+        String temp = words.set(minIndex, words.get(i));
+        words.set(i, temp);
+        minIndex = i + 1;
+       }
+
+       return words;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6};
+        int[] newArr = selectionSort(arr);
+        for(int i = 0; i < newArr.length; i++){
+            System.out.print(newArr[i] + ", ");
+        }
     }
 
     //call this method to load 1000 words into list. Use it to test Part B
